bf23c10cd9fc44cbda7f77f7ab99f2d6034c35b0
Sthiti Deka <dekasthiti@gmail.com>
2015-06-07 18:20:12 -0700

    Module in LLVM API & Module Pass
    Examples for hands-on:
    1. How to write a module in LLVM API
    2. How to write a module pass in LLVM API

--- lib/Transforms/Hello/Hello.cpp
+++ lib/Transforms/Hello/Hello.cpp
@@ -16,11 +16,18 @@
 #include "llvm/IR/Function.h"
 #include "llvm/Pass.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/ADT/DenseMap.h"
+#include "llvm/ADT/SmallString.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/Support/FormattedStream.h"
 using namespace llvm;
 
 #define DEBUG_TYPE "hello"
 
 STATISTIC(HelloCounter, "Counts number of functions greeted");
+STATISTIC(InstructionCounter, "Counts number of functions seen");
 
 namespace {
   // Hello - The first implementation, without getAnalysisUsage.
@@ -63,3 +70,59 @@ namespace {
 char Hello2::ID = 0;
 static RegisterPass<Hello2>
 Y("hello2", "Hello World Pass (with getAnalysisUsage implemented)");
+
+
+namespace {
+    /// CountStaticInstructions: Counts the number of instructions in a module
+    struct CountStaticInstructions : public ModulePass {
+        static char ID; // Pass identification, replacement for typeid
+        DenseMap<const char*, uint> instCountMap;
+        CountStaticInstructions() : ModulePass(ID) {}
+        
+        bool runOnModule(Module &M)
+        {
+            /// Iterate over functions in a module, then, over basic blocks in a function, finally, over instructions
+            /// in a basic block
+            for(Module::iterator MItr = M.begin(); MItr != M.end(); MItr++)
+            {
+                for(Function::iterator FItr = MItr->begin(); FItr != MItr->end(); FItr++)
+                {
+                    errs() << "Basic Block (name = " << FItr->getName() << ") has " << FItr->size() << " instructions.\n";
+                    
+                    for(BasicBlock::iterator BBItr = FItr->begin(); BBItr != FItr->end(); BBItr++)
+                    {
+                        const char* instName  = BBItr->getOpcodeName();
+                        uint        instCount = instCountMap[instName];
+                        
+                        instCountMap[instName] = ++instCount;
+                        
+                    }
+                    
+                    InstructionCounter = InstructionCounter + FItr->size();
+                }
+            }
+            
+            errs() << "Total number of instructions in this module = " << InstructionCounter <<".\n";
+           
+            return false;
+        }
+        
+        
+        void print(raw_ostream& O, const Module* M) const
+        {
+            formatted_raw_ostream Out(O);
+            DenseMap<const char*, uint>::const_iterator mapItr;
+            for(mapItr = instCountMap.begin(); mapItr != instCountMap.end(); mapItr++)
+            {
+                Out.PadToColumn(5)  << mapItr->first;
+                Out.PadToColumn(50) << mapItr->second;
+                Out << "\n";
+            }
+        }
+        
+    };
+}
+
+char CountStaticInstructions::ID = 0;
+static RegisterPass<CountStaticInstructions>
+Z("CountStaticInstructions", "Counts the number of static instructions in a module");
